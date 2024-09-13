8a06ff6186bda076610016b55323f43ee5261cff
Vladislav <vladzhukov59@bk.ru>
2024-09-13 08:37:55 +0300

    Initial commit

---
+++ src/prac1/Main.java
@@ -0,0 +1,12 @@
+package prac1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.println(x);
+
+    }
+}
